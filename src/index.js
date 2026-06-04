b13431a5a3f01b211bc75bfc88f1922d786a963c
kaitlynnnkay <ecommbykaitlyn@gmail.com>
2026-06-04 16:00:49 -0500

    Updated index.js for testing

--- src/index.js
+++ src/index.js
@@ -8,10 +8,16 @@
 // Import the factorial function from the factorial.js file
 const { factorial } = require('./utils/factorial');
 
-// The main() function calculates the factorial of 5 and logs the result to the console
+const { convertToCelsius } = require('./utils/fahrenheit_to_celsius_converter');
+
+// The main() function calculates the factorial of 5 & converts the fahrenheit temp of 50 to celsius, and logs both results to the console
 async function main () {
   const result = factorial(5); // Call the factorial function with the value of 5
   console.log('factorial of 5 is', result); // Log the result to the console
+
+  const fahrenheitTemp = 50; // Fahrenheit temp to convert to Celsius
+  const celsiusTemp = convertToCelsius(fahrenheitTemp); // Convered temperature
+  console.log('the Celsius temperature is:', celsiusTemp); // log to console
 }
 
-main(); // Call the main function to calculate the factorial of 5
+main(); // Call the main function to calculate the factorial of 5 & convert the fahrenheit temp of 50
